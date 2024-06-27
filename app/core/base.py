59717efcc64b9a75ad87176888921f5df898b618
Nurbek878 <nurik878878@yandex.ru>
2024-06-27 11:03:07 +0500

    Add User in models, schemas

--- app/core/base.py
+++ app/core/base.py
@@ -1,5 +1,4 @@
 """Импорты класса Base и всех моделей для Alembic."""
 
 from app.core.db import Base  # noqa
-from app.models.workplace import Workplace  # noqa
-from app.models.booking import Booking  # noqa
+from app.models import Booking, User, Workplace  # noqa
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,3 @@
 from .workplace import Workplace  # noqa
-from .booking import Booking # noqa
+from .booking import Booking  # noqa
+from .user import User  # noqa
